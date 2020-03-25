f7ffd6876efa7e7b5e090fe29369dfe8fc8182b4
David Ibl <david.ibl@xnoname.com>
2020-03-25 09:59:09 +0100

    FIX: Clean up pom and add license

--- dmnmgr/src/main/java/de/lv1871/oss/dmnmgr/App.java
+++ dmnmgr/src/main/java/de/lv1871/oss/dmnmgr/App.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import de.lv1871.oss.dmnmgr.api.documentation.DecisionApi;
@@ -17,7 +16,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @CrossOrigin
 @EnableSwagger2
 @Configuration
-@EnableAspectJAutoProxy
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
